1be04bfe6748f1a6dcc8f9973bdfe27f2d8a22d6
Roger David Cruz <roger.david.cruz@gmail.com>
2019-08-19 10:50:42 -0400

    heroku attempt 1

--- server.js
+++ server.js
@@ -2,6 +2,9 @@
 
 // set up ======================================================================
 // get all the tools we need
+if (process.env.NODE_ENV !== 'production') {
+ require('dotenv').config();
+}
 var express  = require('express');
 var app      = express();
 var port     = process.env.PORT || 9000;
